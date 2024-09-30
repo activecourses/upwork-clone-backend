b943e34d50c2a6a857a8c8513e34b5f567b1fd63
AhmedFatthy1040 <eltonsy1040@outlook.sa>
2024-09-30 10:14:47 +0300

    Add method to UserRepository to find a user by verification token

--- src/main/java/com/activecourses/upwork/repository/UserRepository.java
+++ src/main/java/com/activecourses/upwork/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationToken(String token);
 }
