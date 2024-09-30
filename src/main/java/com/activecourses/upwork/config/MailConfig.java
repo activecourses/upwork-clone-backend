292b38f64521c1ffd64924116e3695826a4ec7fa
AhmedFatthy1040 <eltonsy1040@outlook.sa>
2024-09-30 10:13:34 +0300

    Create MailConfig class to configure JavaMailSender for sending emails

---
+++ src/main/java/com/activecourses/upwork/config/MailConfig.java
@@ -0,0 +1,28 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("test@gmail.com");
+        mailSender.setPassword("test");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
